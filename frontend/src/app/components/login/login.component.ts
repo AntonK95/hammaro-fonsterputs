a0c847083499a53f0a2f04b9437c437fdf797836
Anton Knutsson <cykelkorg95@gmail.com>
2025-03-13 15:04:37 +0100

    quicksave: can now login set token, btn not changing from login to logout

---
+++ frontend/src/app/components/login/login.component.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AuthServiceService } from '../../services/auth.service';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+import { MatDialogRef } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-login',
+  imports: [CommonModule, ReactiveFormsModule],
+  standalone: true,
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent implements OnInit {
+  loginForm!: FormGroup;
+  errorMessage: string | null = null;
+
+
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthServiceService,
+    private router: Router,
+    public dialogRef: MatDialogRef<LoginComponent>
+  ) {
+  }
+  
+  ngOnInit() {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    });
+  }
+
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      this.authService.login(this.loginForm.value).subscribe(
+        (response) => {
+          console.log("Inloggad: ", response);
+          console.log("Angivet värde: ", this.loginForm.value);
+          this.dialogRef.close();
+          // Om vi är staff eller personal så skall vi navigera till deras sida, om kund stanna kvar.
+          // this.router.navigate(['/']);
+          this.loginForm.reset();
+        },
+        (error) => {
+          console.log("Fel vid inloggning: ", error);
+          this.errorMessage = error.error?.error || "Inloggningen misslyckades. Försök igen senare."
+        }
+      )
+    }
+  }
+}
